9bd7c0621c3a21444bd0b2ac43d258f9c94c4f99
haroldwongms <harold.wong@microsoft.com>
2017-09-29 21:28:42 -0700

    disable memory availability check

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -382,7 +382,7 @@ os_sdn_network_plugin_name='redhat/openshift-ovs-multitenant'
 console_port=443
 openshift_cloudprovider_kind=azure
 osm_default_node_selector='type=app'
-openshift_disable_check=docker_image_availability
+openshift_disable_check=memory_availability,docker_image_availability
 
 # default selectors for router and registry services
 openshift_router_selector='type=infra'

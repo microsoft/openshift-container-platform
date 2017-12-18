ab3487f83d441e14a93b3c247cbb78ba7b1bd968
haroldwongms <harold.wong@microsoft.com>
2017-12-18 13:22:46 -0700

    Update to use 3.7

--- scripts/bastionPrep.sh
+++ scripts/bastionPrep.sh
@@ -54,7 +54,7 @@ subscription-manager repos --disable="*"
 subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
-    --enable="rhel-7-server-ose-3.6-rpms" \
+    --enable="rhel-7-server-ose-3.7-rpms" \
     --enable="rhel-7-fast-datapath-rpms"
 
 # Install base packages and update system to latest packages

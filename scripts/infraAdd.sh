2e2428799c67025a8015e3847b04e2c0bc4c8ce3
Vince Power <vince.power@gmail.com>
2018-02-14 18:18:41 -0700

    fixing wget mistake of mine

--- scripts/infraAdd.sh
+++ scripts/infraAdd.sh
@@ -13,7 +13,7 @@ SUDOUSER=$2
 # Run Ansible Playbook to update Hosts file
 
 echo $(date) " - Updating hosts file"
-wget https://raw.githubusercontent.com/microsoft/openshift-container-platform/master/updateinfrahosts.yaml
+wget https://raw.githubusercontent.com/microsoft/openshift-container-platform-playbooks/master/updateinfrahosts.yaml
 ansible-playbook ./updateinfrahosts.yaml
 
 # Run Ansible Playbook to add new Infra to OpenShift Cluster

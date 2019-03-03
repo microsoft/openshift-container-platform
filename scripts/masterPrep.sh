52f5001f029165c844a904d78cd0dc65a0f4cca1
haroldwongms <harold.wong@microsoft.com>
2019-03-03 02:35:40 -0700

    remove unecessary config

--- scripts/masterPrep.sh
+++ scripts/masterPrep.sh
@@ -94,10 +94,10 @@ echo $(date) " - Installing Docker"
 yum -y install docker 
 
 # Update docker storage
-echo "
+# echo "
 # Adding insecure-registry option required by OpenShift
-OPTIONS=\"\$OPTIONS --insecure-registry 172.30.0.0/16\"
-" >> /etc/sysconfig/docker
+# OPTIONS=\"\$OPTIONS --insecure-registry 172.30.0.0/16\"
+# " >> /etc/sysconfig/docker
 
 # Create thin pool logical volume for Docker
 echo $(date) " - Creating thin pool logical volume for Docker and staring service"
